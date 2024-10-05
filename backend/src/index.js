7097a09090dbab5235c3f574a939395d865f061e
Amal Mohammed <amal.cs71215@gmail.com>
2024-10-06 00:49:12 +0530

    Bug fix rollback with logs

--- backend/src/index.js
+++ backend/src/index.js
@@ -9,6 +9,7 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 // Middleware
 app.use(bodyParser.json());
