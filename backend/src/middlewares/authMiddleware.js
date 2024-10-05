0f3efbaef9f93ab3dd7ff2c8260ea204316abe5f
Amal Mohammed <amal.cs71215@gmail.com>
2024-10-06 00:28:32 +0530

    Bug fix for CORS

--- backend/src/middlewares/authMiddleware.js
+++ backend/src/middlewares/authMiddleware.js
@@ -2,13 +2,15 @@
 const admin = require('firebase-admin');
 const User = require('../models/User');
 
+var serviceAccount = require("backend/reviewapp-b1cc2-firebase-adminsdk-o8jzx-a279fe6e21.json");
+
 // Initialize Firebase Admin SDK
-admin.initializeApp({
+admin.initializeApp({ 
   credential: admin.credential.cert({
     type: process.env.FIREBASE_TYPE,
     project_id: process.env.FIREBASE_PROJECT_ID,
     private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,  
-    private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    private_key: process.env.FIREBASE_PRIVATE_KEY,
     client_email: process.env.FIREBASE_CLIENT_EMAIL,
     client_id: process.env.FIREBASE_CLIENT_ID,
     auth_uri: process.env.FIREBASE_AUTH_URI,
